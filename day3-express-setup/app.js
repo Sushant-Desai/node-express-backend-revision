904f421dd95fc17263fb818f216c35fa0e6ede1f
Sushant-Desai <161923974+Sushant-Desai@users.noreply.github.com>
2026-07-01 16:40:21 +0530

    Add dotenv example and process object use

--- day3-express-setup/app.js
+++ day3-express-setup/app.js
@@ -1,3 +1,6 @@
 const express = require("express")
 
 console.log("Express install successfully ");
+
+console.log("Hii I am checking the nodemon package install correctly or not");
+
---
+++ day3-express-setup/dotenv-example.js
@@ -0,0 +1,8 @@
+
+require("dotenv").config();
+
+console.log(process.env.Port);
+
+console.log(process.env.Name);
+
+// console.log(process.env);
