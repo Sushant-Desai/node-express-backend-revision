b2b5966fb7a7ca74ac157580d789e993176a1383
Sushant-Desai <161923974+Sushant-Desai@users.noreply.github.com>
2026-07-06 21:18:17 +0530

    Day 4 - POST Requests and req.body

--- Day-4-Express-Intro/app.js
+++ Day-4-Express-Intro/app.js
@@ -2,118 +2,188 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Home Page")
-    console.log("This is the URL path", req.url);
+// app.get("/", (req, res) => {
+//     res.send("Home Page")
+//     console.log("This is the URL path", req.url);
 
-    console.log("This is the method of url", req.method);
+//     console.log("This is the method of url", req.method);
 
-})
+// })
 
-app.get("/about", (req, res) => {
-    // res.send("About Page")
+// app.get("/about", (req, res) => {
+//     // res.send("About Page")
 
-    console.log("This is the URL path", req.url);
+//     console.log("This is the URL path", req.url);
 
-    console.log("This is the method of url", req.method);
+//     console.log("This is the method of url", req.method);
 
-    // res.send("This About Page ")
+//     // res.send("This About Page ")
 
-    res.json({
+//     res.json({
+//         success: true,
+//         message: "About page load"
+//     })
+// })
+
+// app.get("/contact", (req, res) => {
+//     // res.send("Contact Page")
+
+//     res.status(200).send("Contact page load correctly and All details fetch")
+
+//     console.log("This is the URL path", req.url);
+
+//     console.log("This is the method of url", req.method);
+// })
+
+
+// app.get("/user", (req, res) => {
+
+//     //Combining status + json
+//     res.status(200).json({
+//         success: true,
+//         data: {
+//             id: 1,
+//             name: "Sushant Desai"
+//         }
+//     });
+// });
+
+// // Query Parameter 
+
+// app.get("/users", (req, res) => {
+//     res.json(req.query)
+//     console.log(req.query);
+
+// })
+
+
+
+// // Real Backend Example for query parameter
+// app.get("/users/pagination", (req, res) => {
+//     //   const page = req.query.page;
+//     //   const limit = req.query.limit;
+
+//     // we can convert in number
+//     const page = parseInt(req.query.page);
+//     const limit = parseInt(req.query.limit);
+
+//     console.log(typeof page);
+//     console.log(typeof limit);
+
+
+
+//     res.json({
+//         page,
+//         limit
+//     });
+// });
+
+
+// // Request Parameter 
+// app.get("/user/:id", (req, res) => {
+
+//     //Combining status + json
+//     res.status(200).json({
+//         success: true,
+//         data: {
+//             id: 1,
+//             name: "Sushant Desai"
+//         }
+//     });
+// });
+
+
+// app.get("/users/:id", (req, res) => {
+//     console.log("Checking the route parameters:", req.params);
+
+//     res.send(
+//         `User parameters are ${JSON.stringify(req.params)}\nID is ${req.params.id}`
+//     );
+// });
+
+
+// // Product example
+// app.get("/products/:id", (req, res) => {
+//     const productId = req.params.id;
+
+//     res.json({
+//         success: true,
+//         productId
+//     });
+// });
+
+
+
+// Post method 
+app.use(express.json());
+app.post("/usercreate", (req, res) => {
+
+
+    console.log(req.body);
+
+    // Extract Specific Fields
+    // const name = req.body.name;
+    // const age = req.body.age;
+
+    // Using Destructuring (Preferred)
+    const { name, age } = req.body;
+
+    res.status(200).json({
         success: true,
-        message: "About page load"
+        recieveData: req.body,
+        "Name of Employee is": name,
+        "Age of Employee is ": age
     })
+
+
+
+
+
 })
+// For register users
+app.post("/register", (req, res) => {
 
-app.get("/contact", (req, res) => {
-    // res.send("Contact Page")
+    if (!req.body) {
+        return res.status(400).json({
+            success: false,
+            message: "Request body is missing"
+        });
+    }
 
-    res.status(200).send("Contact page load correctly and All details fetch")
+    const { name, email, password } = req.body;
 
-    console.log("This is the URL path", req.url);
-
-    console.log("This is the method of url", req.method);
-})
-
-
-app.get("/user", (req, res) => {
-
-    //Combining status + json
     res.status(200).json({
         success: true,
-        data: {
-            id: 1,
-            name: "Sushant Desai"
+        message: "User creates successfully",
+        user: {
+            "Name of User is": name,
+            "Email of user is ": email,
+            "password": password
+        }
+    })
+
+})
+
+app.post("/login", (req, res) => {
+    if (!req.body) {
+        return res.status(400).json({
+            success: false,
+            message: "Request body is missing "
+        })
+    }
+
+    const { email, password } = req.body;
+
+
+    res.status(201).json({
+        success: true,
+        message: "User Login successfully",
+        User :{
+            email,
+            password
         }
     });
 });
-
-// Query Parameter 
-
-app.get("/users", (req, res) => {
-    res.json(req.query)
-    console.log(req.query);
-
-})
-
-
-
-// Real Backend Example for query parameter
-app.get("/users/pagination", (req, res) => {
-    //   const page = req.query.page;
-    //   const limit = req.query.limit;
-
-    // we can convert in number
-    const page = parseInt(req.query.page);
-    const limit = parseInt(req.query.limit);
-
-    console.log(typeof page);
-    console.log(typeof limit);
-
-
-
-    res.json({
-        page,
-        limit
-    });
-});
-
-
-// Request Parameter 
-app.get("/user/:id", (req, res) => {
-
-    //Combining status + json
-    res.status(200).json({
-        success: true,
-        data: {
-            id: 1,
-            name: "Sushant Desai"
-        }
-    });
-});
-
-
-app.get("/users/:id", (req, res) => {
-    console.log("Checking the route parameters:", req.params);
-
-    res.send(
-        `User parameters are ${JSON.stringify(req.params)}\nID is ${req.params.id}`
-    );
-});
-
-
-// Product example
-app.get("/products/:id", (req, res) => {
-    const productId = req.params.id;
-
-    res.json({
-        success: true,
-        productId
-    });
-});
-
-
-
 
 app.listen(3000, () => {
     console.log("server is running on port 3000");
