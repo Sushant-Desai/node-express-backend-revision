00e497ad8f43abef40923ff91d0de5d8e166c877
Sushant-Desai <161923974+Sushant-Desai@users.noreply.github.com>
2026-07-29 00:35:56 +0530

    Configure PostgreSQL connection using environment variables

--- Day-08-PostgresSQL/db.js
+++ Day-08-PostgresSQL/db.js
@@ -1,11 +1,23 @@
 const {Pool}=require("pg");
+const dotenv=require('dotenv');
+
+dotenv.config();
+
+
 
 const pool=new Pool({
-    host:"localhost",
-    port:5432,
-    user :"postgres",
-    password:"root",
-    database:"node_backend_db_learning"
+    host:process.env.DB_HOST || "localhost",
+    port:process.env.DB_PORT || 5432,
+    user :process.env.DB_USER || "postgres",
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME||"node_backend_db_learning",
+
+    // Maximum number of database connections in the pool.
+    max:10,
+    // If a connection remains unused for 30 seconds, it is closed automatically.
+    idleTimeoutMillis:30000,
+    // If a new connection cannot be established within 5 seconds, an error is thrown.
+    connectionTimeoutMillis:5000
 });
 
 module.exports=pool;
