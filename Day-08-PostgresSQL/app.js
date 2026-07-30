83b95da9dc67b3279f3f6269187042384e805f93
Sushant-Desai <161923974+Sushant-Desai@users.noreply.github.com>
2026-07-31 00:35:17 +0530

    feat: add PATCH and DELETE user routes

--- Day-08-PostgresSQL/app.js
+++ Day-08-PostgresSQL/app.js
@@ -59,24 +59,84 @@ app.get("/users/:id", async (req, res) => {
     }
 })
 
-// Add user 
 app.post("/users", async (req, res) => {
-    try {
-        const { name } = req.body
+  try {
+    const { name, email } = req.body;
 
-        const result = await pool.query
-            (
-                "Insert into users (name) values ($1)  RETURNING *",
-                [name]
-            );
+    const result = await pool.query(
+      "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *",
+      [name, email]
+    );
 
-        res.status(200).json(result.rows[0])
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
 
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json({
+      message: "Failed to create user",
+    });
+  }
+});
+
+
+// UPDATE user
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email } = req.body;
+
+    const result = await pool.query(
+      `UPDATE users
+       SET name = $1, email = $2
+       WHERE id = $3
+       RETURNING *`,
+      [name, email, id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: "User not found",
+      });
     }
-})
+
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Failed to update user",
+    });
+  }
+});
+
+// DELETE user
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await pool.query(
+      "DELETE FROM users WHERE id = $1 RETURNING *",
+      [id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "User deleted successfully",
+      user: result.rows[0],
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Failed to delete user",
+    });
+  }
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
